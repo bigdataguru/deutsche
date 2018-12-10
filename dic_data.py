fa4f9bf8f2237f819e0dd87b648cea2b966245b3
Stephan Kang <bigdataguru@users.noreply.github.com>
2018-12-09 23:35:57 -0500

    Update dic_data.py

--- dic_data.py
+++ dic_data.py
@@ -1,1 +1,2 @@
-german = {'das Haus':'House', 'gelb':'yellow', 'rot':'red', 'das Decke':'desk'}
+german = {'das Haus':'House', 'gelb':'yellow', 'rot':'red', 'das Decke':'desk','die Bluse':'Blouse', 'das Kleid':'dress','welche':
+        'which', 'der Rock':'skirt','grün':'green','blau':'blue','kurz':'short','der Bleistift':'pencil','der Hut':'hat','schwarz':'black', 'weiß':'white'}
